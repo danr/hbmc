6314afada0c3cb76b57cbd3fbedf34af19793c48
Koen Claessen <koen@chalmers.se>
2015-01-30 17:16:37 +0100

    new, new, new!

---
+++ NewNew.hs
@@ -0,0 +1,579 @@
+{-# LANGUAGE TypeFamilies, GeneralizedNewtypeDeriving, MultiParamTypeClasses, FlexibleInstances, RankNTypes #-}
+module NewNew where
+
+import Control.Applicative
+import Control.Monad
+import Data.IORef
+import Data.List
+import qualified MiniSat as M
+import MiniSat ( Solver, Lit )
+
+--------------------------------------------------------------------------------
+
+data Env =
+  Env
+  { sat   :: Solver
+  , here  :: Bit
+  , waits :: IORef [(Bit,H Bool)]
+  }
+
+newtype H a = H (Env -> IO a)
+
+instance Applicative H where
+  pure  = return
+  (<*>) = ap
+
+instance Functor H where
+  fmap = liftM
+
+instance Monad H where
+  return x  = H (\_   -> return x)
+  H m >>= k = H (\env -> do x <- m env
+                            let H m' = k x
+                            m' env)
+
+--------------------------------------------------------------------------------
+
+run :: H a -> IO a
+run (H m) =
+  M.withNewSolver $ \s ->
+    do ref <- newIORef []
+       m (Env s tt ref)
+
+trySolve :: H (Maybe Bool)
+trySolve = H (\env ->
+  do ws <- readIORef (waits env)
+     putStrLn ("== Try solve with " ++ show (length ws) ++ " waits ==")
+     putStrLn "Finding a counter example..."
+     b <- solveBit (sat env) (here env : [ nt p | (p,_) <- ws ])
+     if b then
+       return (Just True)
+      else
+       do putStrLn "Finding a contradiction..."
+          b <- solveBit (sat env) [here env]
+          if not b then
+            do return (Just False)
+           else
+            let find [] =
+                  do return (error "shouldn't happen")
+                
+                find ((p,H h):ws) =
+                  do b <- solveBit (sat env) [p]
+                     if b then
+                       do chg <- h env{ here = p }
+                          if chg then
+                            do ws1 <- readIORef (waits env)
+                               writeIORef (waits env) (ws1 ++ reverse ws)
+                               putStrLn "!"
+                               return Nothing
+                           else
+                            do putStr "-"
+                               find ws
+                      else
+                       do putStr "X"
+                          find ws
+             
+             in do writeIORef (waits env) []
+                   putStrLn "Finding a point to expand..."
+                   find (reverse ws)
+  )
+
+solve' :: H () -> H Bool
+solve' h =
+  do h
+     mb <- trySolve
+     case mb of
+       Nothing -> solve' h
+       Just b  -> return b
+
+solve :: H Bool
+solve = solve' (return ())
+
+io :: IO a -> H a
+io m = H (\_ -> m)
+
+withSolver :: (Solver -> IO a) -> H a
+withSolver h = H (\env -> h (sat env))
+
+context :: H Bit
+context = H (\env -> return (here env))
+
+withExtraContext :: Bit -> H () -> H ()
+withExtraContext c _ | c == ff = return ()
+withExtraContext c h =
+  do x <- new
+     addClauseHere [nt c, x]
+     c0 <- context
+     inContext x (do addClauseHere [c0]; addClauseHere [c]; h)
+
+inContext :: Bit -> H () -> H ()
+inContext c _ | c == ff = return ()
+inContext c (H m) = H (\env -> m env{ here = c })
+
+later :: H Bool -> H ()
+later h = H (\env ->
+  do ws <- readIORef (waits env)
+     writeIORef (waits env) ((here env, h):ws)
+  )
+
+check :: H () -> H ()
+check h = undefined
+
+must :: Bit -> H () -> H ()
+must c h =
+  do addClauseHere [c]
+     if c == ff then return () else h 
+
+addClauseHere :: [Bit] -> H ()
+addClauseHere xs =
+  do c <- context
+     [c] ==> xs
+
+impossible :: H ()
+impossible = addClauseHere []
+
+noop :: a -> H ()
+noop _ = return ()
+
+choice :: [H ()] -> H ()
+choice [h] = h
+choice hs =
+  do xs <- sequence [ newBit | h <- hs ]
+     addClauseHere xs
+     a <- context
+     sequence_ [ do inContext x (do addClauseHere [a]; h) | (x,h) <- xs `zip` hs ]
+
+ifThenElse :: Bit -> (H (), H ()) -> H ()
+ifThenElse b (yes,no) =
+  choice [ do addClauseHere [b]; yes, do addClauseHere [nt b]; no ]
+
+--------------------------------------------------------------------------------
+
+class Constructive a where
+  newPoint :: Bool -> H a
+
+instance Constructive () where
+  newPoint _ = return ()
+
+instance Constructive Bit where
+  newPoint _ = newBit
+
+instance (Constructive a, Constructive b) => Constructive (a,b) where
+  newPoint inp = liftM2 (,) (newPoint inp) (newPoint inp)
+
+instance Constructive a => Constructive (Maybe a) where
+  newPoint inp = liftM Just (newPoint inp)
+
+new, newInput :: Constructive a => H a
+new      = newPoint False
+newInput = newPoint True
+
+--------------------------------------------------------------------------------
+
+class Equal a where
+  equal        :: a -> a -> H Bit
+  equalHere    :: a -> a -> H ()
+  notEqualHere :: a -> a -> H ()
+
+  equal x y =
+    do q <- newBit
+       inContext q      $ equalHere    x y
+       inContext (nt q) $ notEqualHere x y
+       return q
+
+  equalHere x y =
+    do q <- equal x y
+       addClauseHere [q]
+
+  notEqualHere x y =
+    do q <- equal x y
+       addClauseHere [nt q]
+
+equalPred :: Equal a => a -> a -> Bit -> H ()
+equalPred x y q =
+  do q' <- equal x y
+     equalHere q q'
+
+instance Equal () where
+  equalHere    _ _ = return ()
+  notEqualHere _ _ = addClauseHere []
+
+instance Equal Bit where
+  equalHere x y
+    | x == y    = return ()
+    | x == nt y = addClauseHere []
+    | otherwise = do addClauseHere [nt x, y]
+                     addClauseHere [nt y, x]
+
+  notEqualHere x y = equalHere x (nt y)
+
+instance (Equal a, Equal b) => Equal (a,b) where
+  equalHere (x1,x2) (y1,y2) = 
+    do equalHere x1 y1
+       equalHere x2 y2
+
+  notEqualHere (x1,x2) (y1,y2) = 
+    choice
+    [ notEqualHere x1 y1
+    , notEqualHere x2 y2
+    ]
+
+--------------------------------------------------------------------------------
+
+class Value a where
+  type Type a
+  dflt :: a -> Type a
+  get  :: a -> H (Type a)
+
+instance Value () where
+  type Type () = ()
+  dflt _ = ()
+  get  _ = return ()
+
+instance Value a => Value [a] where
+  type Type [a] = [Type a]
+  dflt _ = []
+  get xs = sequence [ get x | x <- xs ]
+
+instance (Value a, Value b) => Value (a,b) where
+  type Type (a,b) = (Type a, Type b)
+  dflt ~(x,y) = (dflt x, dflt y)
+  get   (x,y) = liftM2 (,) (get x) (get y)
+
+instance Value Bit where
+  type Type Bit = Bool
+  dflt _ = False
+  get (Bool b) = return b
+  get (Lit x)  = do Just b <- withSolver (\s -> M.modelValue s x)
+                    return b
+
+--------------------------------------------------------------------------------
+
+data Bit = Lit Lit | Bool Bool
+ deriving ( Eq, Ord )
+
+instance Show Bit where
+  show (Bool b) = if b then "T" else "F"
+  show (Lit v)  = show v
+
+newBit :: H Bit
+newBit = withSolver (\s -> Lit `fmap` M.newLit s)
+
+ff, tt :: Bit
+ff = Bool False
+tt = Bool True
+
+nt :: Bit -> Bit
+nt (Bool b) = Bool (not b)
+nt (Lit x)  = Lit (M.neg x)
+
+andl, orl :: [Bit] -> H Bit
+andl xs
+  | ff `elem` xs = return ff
+  | otherwise    = withSolver (\s -> andl' s [ x | Lit x <- xs ])
+ where
+  andl' s []  = do return tt
+  andl' s [x] = do return (Lit x)
+  andl' s xs  = do y <- M.newLit s
+                   M.addClause s (y : map M.neg xs)
+                   sequence_ [ M.addClause s [M.neg y, x] | x <- xs ]
+                   return (Lit y)
+
+orl xs = nt `fmap` andl (map nt xs)
+
+addClause :: [Bit] -> H ()
+addClause xs
+  | tt `elem` xs = do return ()
+  | otherwise    = do withSolver (\s -> M.addClause s [ x | Lit x <- xs ])
+                      return ()
+
+(==>) :: [Bit] -> [Bit] -> H ()
+xs ==> ys = addClause (map nt xs ++ ys)
+
+solveBit :: Solver -> [Bit] -> IO Bool
+solveBit s xs | ff `elem` xs =
+  -- should really also set the conflict clause... :-(
+  do return False
+
+solveBit s xs =
+  do M.solve s [ x | Lit x <- xs ]
+
+--------------------------------------------------------------------------------
+
+data Thunk a = This a | Delay Bool (IORef (Either (H ()) a))
+
+this :: a -> Thunk a
+this x = This x
+
+delay :: Bool -> H a -> H (Thunk a)
+delay inp (H m) =
+  do c <- context
+     ref <- io $ newIORef undefined
+     io $ writeIORef ref (Left (H (\env -> m (env{ here = c }) >>= (writeIORef ref . Right))))
+     return (Delay inp ref)
+
+poke :: Thunk a -> H ()
+poke (This _)      = do return ()
+poke (Delay _ ref) =
+  do ema <- io $ readIORef ref
+     case ema of
+       Left m  -> m
+       Right _ -> return ()
+
+peek :: Thunk a -> H (Maybe a)
+peek (This x)      = return (Just x)
+peek (Delay _ ref) =
+  do ema <- io $ readIORef ref
+     return $ case ema of
+       Left _  -> Nothing
+       Right a -> Just a
+
+force :: Thunk a -> H a
+force th =
+  do poke th
+     Just x <- peek th
+     return x
+
+ifForce :: Thunk a -> (a -> H ()) -> H ()
+ifForce (This x)           h = h x
+ifForce th@(Delay inp ref) h =
+  do ema <- io $ readIORef ref
+     case ema of
+       Left m  -> do c <- context
+                     io $ writeIORef ref $ Left $
+                       do m
+                          Just a <- peek th
+                          inContext c (h a)
+                     if inp then
+                       later $
+                         do ma <- peek th
+                            case ma of
+                              Nothing -> do poke th; return True
+                              _       -> do return False
+                      else
+                       return ()
+       Right a -> h a
+
+instance Constructive a => Constructive (Thunk a) where
+  newPoint inp = delay inp (newPoint inp)
+
+instance Equal a => Equal (Thunk a) where
+  equalHere    = zipThunk equalHere
+  notEqualHere = zipThunk notEqualHere
+
+{-
+zipThunk :: (a -> b -> H ()) -> Thunk a -> Thunk b -> H ()
+zipThunk f t1 t2 =
+  do ma <- peek t1
+     mb <- peek t2
+     case (ma, mb) of
+       (Just a, Just b) ->
+         do f a b
+
+       _ ->
+         postpone $
+           do a <- force t1
+              b <- force t2
+              f a b
+              -}
+
+{-
+zipThunk :: (a -> b -> H ()) -> Thunk a -> Thunk b -> H ()
+zipThunk f t1 t2 =
+  do ma <- peek t1
+     mb <- peek t2
+     case (ma, mb) of
+       (Nothing, Nothing) ->
+         postpone $
+           do a <- force t1
+              b <- force t2
+              f a b
+
+       _ ->
+         do a <- force t1
+            b <- force t2
+            f a b
+    -}
+
+zipThunk :: (a -> b -> H ()) -> Thunk a -> Thunk b -> H ()
+zipThunk f t1 t2 =
+  ifForce t1 $ \a ->
+    do b <- force t2
+       f a b
+
+instance Value a => Value (Thunk a) where
+  type Type (Thunk a) = Type a
+
+  dflt x = dflt (unThunk x)
+   where
+    unThunk :: Thunk a -> a
+    unThunk ~(This x) = x
+
+  get (This x)      = get x
+  get (Delay _ ref) =
+    do ema <- io $ readIORef ref
+       case ema of
+         Left _  -> return (dflt (either undefined id ema))
+         Right x -> get x
+
+--------------------------------------------------------------------------------
+
+newtype Val a = Val [(Bit,a)] -- sorted on a
+ deriving ( Eq, Ord )
+
+instance Show a => Show (Val a) where
+  show (Val xs) = concat (intersperse "|" [ show x | (_,x) <- xs ])
+
+val :: a -> Val a
+val x = Val [(tt,x)]
+
+(=?) :: Eq a => Val a -> a -> Bit
+Val []         =? x  = ff
+Val ((a,y):xs) =? x
+  | x == y      = a
+  | otherwise   = Val xs =? x
+
+domain :: Val a -> [a]
+domain (Val xs) = map snd xs
+
+newVal :: Ord a => [a] -> H (Val a)
+newVal xs =
+  do as <- lits (length ys)
+     return (Val (as `zip` ys))
+ where
+  ys = map head . group . sort $ xs
+
+  lits 1 =
+    do return [tt]
+
+  lits 2 =
+    do a <- newBit
+       return [a,nt a]
+
+  lits n =
+    do as <- sequence [ newBit | i <- [1..n] ]
+       addClause as
+       atMostOne n as
+       return as
+
+  atMostOne n as | n <= 5 =
+    do sequence_ [ addClause [nt x, nt y] | (x,y) <- pairs as ]
+   where
+    pairs (x:xs) = [ (x,y) | y <- xs ] ++ pairs xs
+    pairs _      = []
+
+  atMostOne n as =
+    do a <- newBit
+       atMostOne (k+1) (a : take k as)
+       atMostOne (n-k+1) (nt a : drop k as)
+   where
+    k = n `div` 2
+
+instance Ord a => Equal (Val a) where
+  equalHere (Val xs) (Val ys) = eq xs ys
+   where
+    eq [] ys = sequence_ [ addClauseHere [nt y] | (y,_) <- ys ]
+    eq xs [] = sequence_ [ addClauseHere [nt x] | (x,_) <- xs ]
+    eq ((x,a):xs) ((y,b):ys) =
+      case a `compare` b of
+        LT -> do addClauseHere [nt x]
+                 eq xs ((y,b):ys)
+        EQ -> do addClauseHere [nt x, y]
+                 eq xs ys
+        GT -> do addClauseHere [nt y]
+                 eq ((x,a):xs) ys
+  
+  notEqualHere (Val xs) (Val ys) = neq xs ys
+   where
+    neq [] ys = return ()
+    neq xs [] = return ()
+    neq ((x,a):xs) ((y,b):ys) =
+      case a `compare` b of
+        LT -> do neq xs ((y,b):ys)
+        EQ -> do addClauseHere [nt x, nt y]
+                 neq xs ys
+        GT -> do neq ((x,a):xs) ys
+  
+instance Value (Val a) where
+  type Type (Val a) = a
+  
+  dflt _ = error "no default value for Val" -- URK
+  
+  get (Val xs) =
+    do bs <- sequence [ get x | (x,_) <- xs ]
+       return (head [ a | (True,(_,a)) <- bs `zip` xs ])
+
+--------------------------------------------------------------------------------
+
+data Data c a = Con (Val c) a
+
+con :: Ord c => c -> a -> Thunk (Data c a)
+con c a = this (Con (val c) a)
+
+proj :: Maybe a -> (a -> H ()) -> H ()
+proj (Just x) h = h x
+proj _        _ = return ()
+
+proj1 :: (Maybe a, z) -> (a -> H ()) -> H ()
+proj1 (Just x, _) h = h x
+proj1 _           _ = return ()
+
+proj2 :: (x, (Maybe a, z)) -> (a -> H ()) -> H ()
+proj2 (_, (Just x, _)) h = h x
+proj2 _                _ = return ()
+
+proj3 :: (x, (y, (Maybe a, z))) -> (a -> H ()) -> H ()
+proj3 (_, (_, (Just x, _))) h = h x
+proj3 _                     _ = return ()
+
+class (Show c, Ord c) => ConstructiveData c where
+  constrs :: [c]
+
+instance (ConstructiveData c, Constructive a) => Constructive (Data c a) where
+  newPoint inp = do vc <- newVal constrs
+                    a  <- newPoint inp
+                    return (Con vc a)
+
+class ConstructiveData c => EqualData c a where
+  equalData :: (forall x . Equal x => x -> x -> H ()) ->
+               [([c], a -> a -> H ())]
+
+instance EqualData c a => Equal (Data c a) where
+  equalHere (Con c1 x1) (Con c2 x2) =
+    do equalHere c1 c2
+       c <- context
+       sequence_
+         [ do x <- new
+              sequence_ [ addClauseHere [nt (c1 =? c), x] | c <- cs ]
+              inContext x (do addClauseHere [c]; f x1 x2)
+         | (cs, f) <- equalData equalHere
+         , any (`elem` allcs) cs
+         ]
+   where
+    allcs = domain c1 `intersect` domain c2
+
+  notEqualHere (Con c1 x1) (Con c2 x2) =
+    choice
+    [ do notEqualHere c1 c2
+    , do equalHere c1 c2
+         choice
+           [ do addClauseHere [ c1 =? c | c <- cs ]
+                f x1 x2
+           | (cs, f) <- equalData notEqualHere
+           , any (`elem` allcs) cs
+           ]
+     ]
+   where
+    allcs = domain c1 `intersect` domain c2
+
+getData :: (c -> a -> H b) -> b -> Thunk (Data c a) -> H b
+getData f d t =
+  do md <- peek t
+     case md of
+       Nothing -> return d
+       Just (Con c a) ->
+         do x <- get c
+            f x a
+
+--------------------------------------------------------------------------------
+
---
+++ NewNewExamples.hs
@@ -0,0 +1,379 @@
+{-# LANGUAGE TypeFamilies, GeneralizedNewtypeDeriving, MultiParamTypeClasses, FlexibleInstances, RankNTypes #-}
+module Main where
+
+import NewNew
+import Data.List ( intersperse )
+
+--------------------------------------------------------------------------------
+
+newtype List a = List (Thunk (Data L (Maybe (a, List a))))
+ deriving ( Constructive, Equal )
+
+data L = Nil | Cons
+ deriving ( Eq, Ord, Show )
+
+nil       = List (con Nil Nothing)
+cons x xs = List (con Cons (Just (x, xs)))
+
+caseList (List xs) h = ifForce xs h
+makeList (List xs) h = do a <- force xs; h a
+
+instance ConstructiveData L where
+  constrs = [Nil, Cons]
+
+instance Equal a => EqualData L (Maybe (a, List a)) where
+  equalData h =
+    [ ([Cons], \(Just (x,_))  (Just (y,_))  -> h x y)
+    , ([Cons], \(Just (_,xs)) (Just (_,ys)) -> h xs ys)
+    ]
+
+instance Value a => Value (List a) where
+  type Type (List a) = [Type a]
+  
+  dflt _ = []
+
+  get (List t) = getData f [] t
+   where
+    f Nil  _        = return []
+    f Cons (Just a) = do (x,xs) <- get a; return (x:xs)
+
+--------------------------------------------------------------------------------
+
+app xs ys zs =
+  caseList xs $ \(Con cxs axs) ->
+    choice
+    [ must (cxs =? Nil) $
+        equalHere ys zs
+
+    , must (cxs =? Cons) $
+        proj axs $ \(a,as) ->
+          makeList zs $ \(Con czs azs) ->
+            must (czs =? Cons) $
+              proj azs $ \(c,cs) ->
+                do equalHere a c
+                   app as ys cs
+    ]
+
+--------------------------------------------------------------------------------
+
+newtype Nat = Nat (Thunk (Data N (Maybe Nat)))
+ deriving ( Constructive, Equal )
+
+data N = Zero | Succ
+ deriving ( Eq, Ord, Show )
+
+zer   = Nat (con Zero Nothing)
+suc n = Nat (con Succ (Just n))
+
+nat 0 = zer
+nat n = suc (nat (n-1))
+
+caseNat (Nat n) h = ifForce n h
+makeNat (Nat n) h = do x <- force n; h x
+
+instance ConstructiveData N where
+  constrs = [Zero, Succ]
+
+instance EqualData N (Maybe Nat) where
+  equalData h = [([Succ], \(Just t1) (Just t2) -> h t1 t2)]
+
+instance Value Nat where
+  type Type Nat = Integer
+  
+  dflt _ = 0
+
+  get (Nat t) = getData f 0 t
+   where
+    f Zero _        = return 0
+    f Succ (Just a) = do n <- get a; return (n+1)
+
+--------------------------------------------------------------------------------
+
+add x y z =
+  caseNat x $ \(Con cx ax) ->
+    choice
+    [ must (cx =? Zero) $
+        equalHere y z
+    
+    , must (cx =? Succ) $
+        proj ax $ \a ->
+          makeNat z $ \(Con cz az) ->
+            must (cz =? Succ) $
+              proj az $ \c ->
+                add a y c
+    ]
+
+--------------------------------------------------------------------------------
+
+newtype RE a = RE (Thunk (Data R (Maybe a, (Maybe (RE a), (Maybe (RE a), ())))))
+ deriving ( Constructive, Equal )
+ 
+data R = RNil | REps | RAtom | RPlus | RAnd | RSeq | RStar
+ deriving ( Eq, Ord, Show )
+
+rnil      = RE (con RNil (Nothing, (Nothing, (Nothing, ()))))
+reps      = RE (con REps (Nothing, (Nothing, (Nothing, ()))))
+ratom a   = RE (con RAtom (Just a, (Nothing, (Nothing, ()))))
+rplus p q = RE (con RPlus (Nothing, (Just p, (Just q, ()))))
+rand  p q = RE (con RAnd  (Nothing, (Just p, (Just q, ()))))
+rseq  p q = RE (con RSeq  (Nothing, (Just p, (Just q, ()))))
+rstar p   = RE (con RStar (Nothing, (Just p, (Nothing, ()))))
+
+caseRE (RE r) h = ifForce r h
+makeRE (RE r) h = force r >>= h
+
+instance ConstructiveData R where
+  constrs = [RNil, REps, RAtom, RPlus, RAnd, RSeq, RStar]
+
+instance Equal a => EqualData R (Maybe a, (Maybe (RE a), (Maybe (RE a), ()))) where
+  equalData h =
+    [ ([RAtom],                 \(Just x,_)         (Just y,_)         -> h x y)
+    , ([RPlus,RAnd,RSeq,RStar], \(_,(Just p,_))     (_,(Just q,_))     -> h p q)
+    , ([RPlus,RAnd,RSeq],       \(_,(_,(Just p,_))) (_,(_,(Just q,_))) -> h p q)
+    ]
+
+data EXP a = NIL | EPS | ATOM a | EXP a :+: EXP a | EXP a :&: EXP a | EXP a :>: EXP a | STAR (EXP a) deriving ( Show )
+
+instance Value a => Value (RE a) where
+  type Type (RE a) = EXP (Type a)
+  
+  dflt _ = NIL
+
+  get (RE t) = getData f NIL t
+   where
+    f RNil  _         = return NIL
+    f REps  _         = return EPS
+    f RAtom (Just x,_) = do a <- get x; return (ATOM a)
+    f RPlus (_,pq)    = do (x,y) <- get' pq; return (x :+: y)
+    f RAnd  (_,pq)    = do (x,y) <- get' pq; return (x :&: y)
+    f RSeq  (_,pq)    = do (x,y) <- get' pq; return (x :>: y)
+    f RStar (_,(Just p,_)) = do x <- get p; return (STAR x)
+
+    get' (Just p, (Just q, _)) = get (p,q)
+
+--------------------------------------------------------------------------------
+
+step p a r =
+  caseRE p $ \(Con cp ap) ->
+    do c <- context
+     
+       rec1 <- new
+       r1 <- new
+       inContext rec1 $
+         do addClauseHere [c]
+            proj2 ap $ \p' -> step p' a r1
+     
+       rec2 <- new
+       r2 <- new
+       inContext rec2 $
+         do addClauseHere [c]
+            proj3 ap $ \q' -> step q' a r2
+     
+       choice
+         [ must (cp =? RNil) $
+             do addClauseHere [nt rec1]
+                addClauseHere [nt rec2]
+                makeRE r $ \(Con cr ar) ->
+                  must (cr =? RNil) $
+                    return ()
+
+         , must (cp =? REps) $
+             do addClauseHere [nt rec1]
+                addClauseHere [nt rec2]
+                makeRE r $ \(Con cr ar) ->
+                  must (cr =? RNil) $
+                    return ()
+         
+         , must (cp =? RAtom) $
+             proj1 ap $ \b ->
+               do addClauseHere [nt rec1]
+                  addClauseHere [nt rec2]
+                  choice
+                    [ do equalHere a b
+                         makeRE r $ \(Con cr ar) ->
+                           must (cr =? REps) $
+                             return ()
+                    , do notEqualHere a b
+                         makeRE r $ \(Con cr ar) ->
+                           must (cr =? RNil) $
+                             return ()
+                    ]
+
+         , must (cp =? RPlus) $
+             do addClauseHere [rec1]
+                addClauseHere [rec2]
+                makeRE r $ \(Con cr ar) ->
+                  must (cr =? RPlus) $
+                    proj2 ar $ \w1 ->
+                      proj3 ar $ \w2 ->
+                        do equalHere r1 w1
+                           equalHere r2 w2
+         
+         , must (cp =? RAnd) $
+             do addClauseHere [rec1]
+                addClauseHere [rec2]
+                makeRE r $ \(Con cr ar) ->
+                  must (cr =? RAnd) $
+                    proj2 ar $ \w1 ->
+                      proj3 ar $ \w2 ->
+                        do equalHere r1 w1
+                           equalHere r2 w2
+         
+         , must (cp =? RSeq) $
+            proj2 ap $ \p1 ->
+            proj3 ap $ \p2 ->
+             do addClauseHere [rec1]
+                addClauseHere [rec2]
+                b <- new
+                eps p1 b
+                choice
+                  [ do addClauseHere [nt b]
+                       makeRE r $ \(Con cr ar) ->
+                         must (cr =? RSeq) $
+                           proj2 ar $ \w1 ->
+                             proj3 ar $ \w2 ->
+                               do equalHere r1 w1
+                                  equalHere p2 w2
+                  
+                  , do addClauseHere [b]
+                       makeRE r $ \(Con cr ar) ->
+                         must (cr =? RPlus) $
+                           proj2 ar $ \w1 ->
+                             proj3 ar $ \w2 ->
+                               makeRE w1 $ \(Con cw1 aw1) ->
+                                 must (cw1 =? RSeq) $
+                                   proj2 aw1 $ \v1 ->
+                                     proj3 aw1 $ \v2 ->
+                                       do equalHere r1 v1
+                                          equalHere p2 v2
+                                          equalHere r2 w2
+                  ]
+
+         , must (cp =? RStar) $
+             do addClauseHere [rec1]
+                addClauseHere [nt rec2]
+                makeRE r $ \(Con cr ar) ->
+                  must (cr =? RSeq) $
+                    proj2 ar $ \w1 ->
+                      proj3 ar $ \w2 ->
+                        do equalHere r1 w1
+                           equalHere p w2
+         ]
+
+eps p b =
+  caseRE p $ \(Con cp ap) ->
+    do c <- context
+     
+       rec1 <- new
+       r1 <- new
+       inContext rec1 $
+         do addClauseHere [c]
+            proj2 ap $ \p' -> eps p' r1
+     
+       rec2 <- new
+       r2 <- new
+       inContext rec2 $
+         do addClauseHere [c]
+            proj3 ap $ \q' -> eps q' r2
+     
+       choice
+         [ must (cp =? RNil) $
+             do addClauseHere [nt rec1]
+                addClauseHere [nt rec2]
+                addClauseHere [nt b]
+
+         , must (cp =? REps) $
+             do addClauseHere [nt rec1]
+                addClauseHere [nt rec2]
+                addClauseHere [b]
+         
+         , must (cp =? RAtom) $
+             do addClauseHere [nt rec1]
+                addClauseHere [nt rec2]
+                addClauseHere [nt b]
+
+         , must (cp =? RPlus) $
+             do addClauseHere [rec1]
+                addClauseHere [rec2]
+                addClauseHere [nt b, r1, r2]
+                addClauseHere [nt r1, b]
+                addClauseHere [nt r2, b]
+         
+         , must (cp =? RAnd) $
+             do addClauseHere [rec1]
+                addClauseHere [rec2]
+                addClauseHere [b, nt r1, nt r2]
+                addClauseHere [r1, nt b]
+                addClauseHere [r2, nt b]
+         
+         , must (cp =? RSeq) $
+             do addClauseHere [rec1]
+                addClauseHere [rec2]
+                addClauseHere [b, nt r1, nt r2]
+                addClauseHere [r1, nt b]
+                addClauseHere [r2, nt b]
+
+         , must (cp =? RStar) $
+             do addClauseHere [rec1]
+                addClauseHere [nt rec2]
+                addClauseHere [b]
+         ]
+
+rec p s b =
+  caseList s $ \(Con cs as) ->
+    choice
+    [ must (cs =? Nil) $
+        eps p b
+  
+    , must (cs =? Cons) $
+        proj as $ \(a,s') ->
+          do q <- new
+             step p a q
+             rec q s' b
+    ]
+
+newtype CHAR = CHAR (Val Char)
+ deriving ( Equal )
+
+instance Value CHAR where
+  type Type CHAR = Char
+  
+  dflt _ = '?'
+  
+  get (CHAR v) = get v
+
+instance Constructive CHAR where
+  newPoint _ = CHAR `fmap` newVal "ABC"
+
+main = run $
+  do p <- newInput :: H (RE CHAR)
+     q <- newInput :: H (RE CHAR)
+     s <- newInput :: H (List CHAR)
+     --a <- new
+     --b <- new
+     --let s = cons a (cons b nil)
+
+     --rec2 (p `rseq` q) s ff
+     --rec2 (q `rseq` p) s tt
+     
+     --let p = ratom (CHAR (val 'A'))
+     --let q = ratom (CHAR (val 'B'))
+     
+     rec (rstar p `rseq` rstar q) s ff
+     rec (rstar (p `rplus` q)) s tt
+     --rec2 (rstar p `rseq` rstar q) s ff
+     --rec2 (rstar (p `rplus` q)) s tt
+     --rec (rstar (p `rplus` q)) s
+     --eps p ff
+     --rec (p `rand` (p `rseq` p)) s
+     
+     
+     let see = ((p,q),s)
+     io $ putStrLn "Solving..."
+     b <- solve
+     io $ print b
+     if b then
+       do get see >>= (io . print)
+      else
+       do return ()
+
