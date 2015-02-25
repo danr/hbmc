a2ae9fe5d688edc951a597c062d854e1e74774fb
Dan Rosén <danr@chalmers.se>
2015-02-25 12:28:58 +0100

    Compare with LazySmallCheck

---
+++ comparison/Nat.hs
@@ -0,0 +1,48 @@
+module Nat where
+
+import Prelude hiding ((+),(*),(-),(<))
+import Tip.DSL
+
+data Nat = Z | S Nat deriving (Eq,Ord,Show)
+
+infixl 6 -
+infixl 6 +
+infixl 7 *
+
+(+) :: Nat -> Nat -> Nat
+S n + m = S (n + m)
+Z   + m = m
+
+(*) :: Nat -> Nat -> Nat
+S n * m = m + (n * m)
+Z   * m = Z
+
+-- | Truncated subtraction
+(-) :: Nat -> Nat -> Nat
+S n - S m = n - m
+S m - Z   = S m
+Z   - Z   = Z
+Z   - _   = Z
+
+(<) :: Nat -> Nat -> Bool
+Z < _     = True
+S{} < Z   = False
+S n < S m = n < m
+
+(===) :: Nat -> Nat -> Bool
+Z   === Z   = True
+Z{} === S{} = False
+S{} === Z{} = False
+S n === S m = n === m
+
+-- plus_idem x = x + x =:= x
+-- mul_idem  x = x * x =:= x
+-- plus_not_idem x = x + x =:= x ==> True =:= False
+
+silly x y z = x * (y + z) =:= (x * y) + z
+
+sub_comm  x y   = x - y =:= y - x
+sub_assoc x y z = x - (y - z) =:= (x - y) - z
+
+not_trans x y z = x < y =:= True ==> y < z =:= True ==> x < z =:= False
+
