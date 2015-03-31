50541ef9145c9140438126f4c3172ce015af0931
Dan Rosén <danr@chalmers.se>
2015-03-31 10:40:33 +0200

    Checkout right commit of tip lib, for now

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/augustss/geniplate.git
 (cd geniplate; cabal install)
 git clone https://github.com/tip-org/tools
-(cd tools; cabal install tip-lib/ tip-haskell-frontend/)
+(cd tools; git checkout ef4241f7b9703d09c2fd12bee22eef46bdaf2d1a; cabal install tip-lib/ tip-haskell-frontend/)
 cabal install
